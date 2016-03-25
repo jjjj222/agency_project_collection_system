b40f4ff0023c2ef6cb3f389963d63796adc9f107
mynetidfornow <mynetidfornow@tamu.edu>
2016-03-25 20:47:25 +0000

    added first rspec test for user
    * should be a student or a prof, but not both

--- spec/models/tamu_user_spec.rb
+++ spec/models/tamu_user_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe TamuUser, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should be either a student or a professor" do
+    user = TamuUser.new
+    user.should satisfy { |u| u.is_student? ^ u.is_professor? }
+  end
 end
