c15dc0735e9c1fcb5e14fd765964c9cb0dd1f539
mynetidfornow <mynetidfornow@tamu.edu>
2016-03-25 22:33:23 +0000

    add belong_to, has_and_belongs_to_many by hand?
    * not sure if we have to do this...

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,9 @@ class Project < ActiveRecord::Base
     ["completed","in progress","cancelled","open"]
   end
 
+  belongs_to :agency
+  has_and_belongs_to_many :project
+
   validates :status, :inclusion => { :in => Project.all_statuses }
   serialize :tags, Array
 end
