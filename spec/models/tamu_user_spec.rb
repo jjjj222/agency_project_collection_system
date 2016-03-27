d182f3c580c2f2e975d3990f6d668015656b64cb
Brennen <brtaylor1001@gmail.com>
2016-03-27 21:00:58 +0000

    Add FactoryGirl gem. Changed tests for user model to use factory girl

--- spec/models/tamu_user_spec.rb
+++ spec/models/tamu_user_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.describe TamuUser, type: :model do
   
   it 'is invalid without name' do
-      expect(TamuUser.new(:email => 'test@test.com', :role => "student")).to_not be_valid
+      # expect(TamuUser.new(:email => 'test@test.com', :role => "student")).to_not be_valid
+      expect(FactoryGirl.build(:tamu_user, :email, :student)).to_not be_valid
   end
   
   it 'is invalid without email' do
-      expect(TamuUser.new(:name => 'John Smith', :role => "student")).to_not be_valid
+      expect(FactoryGirl.build(:tamu_user, :name, :student)).to_not be_valid
   end
   
   it 'is invalid without correctly formatted email' do
-      expect(TamuUser.new(:name => 'John Smith', :email => 'testEmail', :role => "student")).to_not be_valid
+      expect(FactoryGirl.build(:tamu_user, :default, :email => "badEmail")).to_not be_valid
   end
   
   it "is invalid without a role" do
-    expect(TamuUser.new(:name => 'John Smith', :email => 'test@gmail.com')).to_not be_valid
+    expect(FactoryGirl.build(:tamu_user, :name, :email)).to_not be_valid
   end
   
   it "has and belongs to many projects" do
@@ -24,31 +25,31 @@ RSpec.describe TamuUser, type: :model do
   end
 
   it "should be either a student or a professor" do
-    user = TamuUser.new(:name => 'John Smith', :email => "test@gmail.com", :role => "student")
+    user = FactoryGirl.build(:tamu_user, :default)
     user.should satisfy { |u| u.is_student? ^ u.is_professor? }
   end
   
   it "should fail to validate a bad role" do
-    expect(TamuUser.new(:name => 'John Smith', :email => "test@gmail.com", :role => "badRole")).to_not be_valid
+    expect(FactoryGirl.build(:tamu_user, :default, :role => "badRole")).to_not be_valid
   end
   
   it "Ensure creating as a student is a student" do
-    user = TamuUser.new(:name => 'John Smith', :email => "test@gmail.com", :role => "student")
+    user = FactoryGirl.build(:tamu_user, :default, :student)
     expect(user).to be_is_student
   end
   
   it "Ensure creating as a prof is a prof" do
-    user = TamuUser.new(:name => 'John Smith', :email => "test@gmail.com", :role => "professor")
+    user = FactoryGirl.build(:tamu_user, :default, :professor)
     expect(user).to be_is_professor
   end
   
   it 'Test is_professor? method' do
-    user = TamuUser.new(:name => 'John Smith', :email => "test@gmail.com", :role => "student")
+    user = FactoryGirl.build(:tamu_user, :default, :student)
     expect(user).to_not be_is_professor
   end
   
   it 'Test is_student? method' do
-    user = TamuUser.new(:name => 'John Smith', :email => "test@gmail.com", :role => "professor")
+    user = FactoryGirl.build(:tamu_user, :default, :professor)
     expect(user).to_not be_is_student
   end
 
