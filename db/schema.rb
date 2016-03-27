7dc50e7bbde703708a3a8ca4bf767d7d3cfaac9f
Brennen <brtaylor1001@gmail.com>
2016-03-27 20:02:56 +0000

    Added role status to tamu user table. Generated and ran migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325222311) do
+ActiveRecord::Schema.define(version: 20160327200020) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160325222311) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "role"
   end
 
 end
