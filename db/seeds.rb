fa075b452c156c5184b604321243f3dd8346077c
mynetidfornow <mynetidfornow@tamu.edu>
2016-03-28 23:06:48 +0000

    add agency seed data and agency_id to proj seed
    * currently just dumps agency object rather than name bc creating
      project currently doesn't supply an id

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 example_projects = [
-  { name: "Test Project", description: "A completed test project", status: "completed"}
+  { name: "Test Project", description: "A completed test project", status: "completed", agency_id: 1}
 ]
 
 example_users = [
@@ -14,6 +14,9 @@ example_users = [
   { name: "Hank Tamu", email: "hank@tamu.edu", role: "professor" }
 ]
 
+example_agencies = [
+  { name: "Some nonprofit", email: "noprofit@gmail.com", phone_number: "979-867-5309" }
+]
 
 example_projects.each do |proj|
   Project.create!(proj)
@@ -21,4 +24,8 @@ end
 
 example_users.each do |user|
   TamuUser.create!(user)
+end
+
+example_agencies.each do |agency|
+  Agency.create!(agency)
 end
