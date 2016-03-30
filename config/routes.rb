c43c068e7fe808431fc9ef6ddd7c9e8fe86982a3
jjjj222 <jjjj222.tso@gmail.com>
2016-03-30 10:59:27 -0500

    setup environment for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # get 'tamu_users/index'
 
   get 'welcome/index'
@@ -13,6 +15,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :agencies
+
+  get       'login'     => 'sessions#new'
+  post      'login'     => 'sessions#create'
+  delete    'logout'    => 'sessions#destroy'
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
