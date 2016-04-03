5875957ad7161369001c28fee78e91355ecbe2da
Evan Feiereisel <cloud9@feiereisel.us>
2016-04-03 20:49:36 +0000

    added unapproved projects view and controller method; updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 example_projects = [
-  { name: "Test Project", description: "A completed test project", status: "completed"},
-  { name: "Test Project 2", description: "BCD", status: "completed"}
+  { name: "Test Project", description: "A completed test project", status: "completed", approved: true},
+  { name: "Test Project 2", description: "BCD", status: "completed", approved: true},
+  { name: "Test Project 3", description: "BCD", status: "open", approved: false},
+  { name: "Test Project 4", description: "BCD", status: "in progress", approved: false}
 ]
 
 example_users = [
-  { name: "John Tamu", email: "john@tamu.edu", role: "student"},
-  { name: "Hank Tamu", email: "hank@tamu.edu", role: "professor"}
+  { name: "John Tamu", email: "john@tamu.edu", role: "student", admin: false},
+  { name: "Hank Tamu", email: "hank@tamu.edu", role: "professor", admin: false},
+  { name: "Malini Malini", email: "malini@tamu.edu", role: "professor", admin: true}
+  
 ]
 
 example_agencies = [
-  { name: "Some nonprofit", email: "noprofit@gmail.com", phone_number: "979-867-5309" },
-  { name: "Another nonprofit", email: "noprofit2@gmail.com", phone_number: "979-555-5555" }
+  { name: "Some nonprofit", email: "noprofit@gmail.com", phone_number: "979-867-5309", approved: true },
+  { name: "Another nonprofit", email: "noprofit2@gmail.com", phone_number: "979-555-5555", approved: false}
 ]
+
 
 example_projects.each do |proj|
   Project.create!(proj)
