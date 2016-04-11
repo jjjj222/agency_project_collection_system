edb41aec655f663fb04e6be94bba8c0f3ff43960
Daniel Latypov <dlatypov@lily>
2016-04-11 11:05:55 -0500

    100% view coverage, fix proj.new/edit
    * edit/new for project also used plural when they shouldn't have

--- spec/views/projects/edit.html.haml_spec.rb
+++ spec/views/projects/edit.html.haml_spec.rb
@@ -8,33 +8,25 @@ describe 'projects/edit.html.haml' do
             assign :project, @project
             render
         end
+
+        it 'has a form for editing the project' do
+            expect(rendered).to have_selector('form#edit_project_form')
+        end
         
         it 'displays a text field for name' do
-            expect(rendered).to have_selector('form#edit_project_form') do |form|
-              expect(form).to have_selector('textarea',
-                                        :name => 'project[name]')
-            end
+            expect(rendered).to have_field("Name", with: @project.name)
+        end
+
+        it 'has a field for description' do
+            expect(rendered).to have_field("Description", with: @project.description)
         end
         
-        it 'displays a text field for description' do
-            expect(rendered).to have_selector('form#edit_project_form') do |form|
-              expect(form).to have_selector('textarea',
-                                        :name => 'project[description]')
-            end
+        it 'has a field for tags' do
+            expect(rendered).to have_field("Tags", with: @project.tags.join(", "))
         end
         
-        it 'displays a text field for tags' do
-            expect(rendered).to have_selector('form#edit_project_form') do |form|
-              expect(form).to have_selector('textarea',
-                                        :name => 'project[tags]')
-            end
-        end
-        
-        it 'displays a text field for status' do
-            expect(rendered).to have_selector('form#edit_project_form') do |form|
-              expect(form).to have_selector('textarea',
-                                        :name => 'project[status]')
-            end
+        it 'has a field for status' do
+            expect(rendered).to have_field("Status", with: @project.status)
         end
     end
 end
--- spec/views/projects/new.html.haml_spec.rb
+++ spec/views/projects/new.html.haml_spec.rb
@@ -8,34 +8,27 @@ describe 'projects/new.html.haml' do
             assign :project, @project
             render
         end
+
+        it 'has a form for a new project' do
+            expect(rendered).to have_selector('form#new_project_form')
+        end
         
         it 'displays a text field for name' do
-            expect(rendered).to have_selector('form#new_project_form') do |form|
-              expect(form).to have_selector('textarea',
-                                        :name => 'project[name]')
-            end
+            expect(rendered).to have_field("Name", with: @project.name)
+        end
+
+        it 'has a field for description' do
+            expect(rendered).to have_field("Description", with: @project.description)
         end
         
-        it 'displays a text field for description' do
-            expect(rendered).to have_selector('form#new_project_form') do |form|
-              expect(form).to have_selector('textarea',
-                                        :name => 'project[description]')
-            end
+        it 'has a field for tags' do
+            expect(rendered).to have_field("Tags", with: @project.tags.join(", "))
         end
         
-        it 'displays a text field for tags' do
-            expect(rendered).to have_selector('form#new_project_form') do |form|
-              expect(form).to have_selector('textarea',
-                                        :name => 'project[tags]')
-            end
-        end
-        
-        it 'displays a hidden field for status' do
-            expect(rendered).to have_selector('form#new_project_form') do |form|
-              expect(form).to have_selector('hidden_field',
-                                        :name => 'project[status]',
-                                        :value => 'open')
-            end
+        it 'displays a hidden field for status with value of open' do
+            expect(rendered).to have_selector('#project_status', visible: false)
+            # Hackish way to test value on hidden fields, would be nice to replace
+            expect(rendered).to have_selector("input#project_status[value=\"open\"]")
         end
     end
 end
