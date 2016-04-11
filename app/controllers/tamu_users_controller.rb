a3bdc276dc3914af9964be60b5a63dde3523e0a0
Daniel Latypov <dlatypov@lily>
2016-04-11 00:20:41 -0500

    unescape html in flash, add <br> to validation msg
    * != reads the result as html, unsanitized

--- app/controllers/tamu_users_controller.rb
+++ app/controllers/tamu_users_controller.rb
@@ -45,7 +45,7 @@ class TamuUsersController < ApplicationController
             # redirect_to :action => 'edit', id: @tamu_user.id
         else
           if @tamu_user.errors.any?
-            flash[:notice] = @tamu_user.errors.full_messages.join(". ")
+            flash[:notice] = @tamu_user.errors.full_messages.join("<br>")
           else
             flash[:notice] = "Failed"
           end
