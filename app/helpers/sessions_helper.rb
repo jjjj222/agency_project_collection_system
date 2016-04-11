3c5327b2454288536a65d442f2634fefed9bd913
Daniel Latypov <dlatypov@lily>
2016-04-11 14:30:04 -0500

    spec for session_helper, default type for log_in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
-  def log_in(user, type)
+  def log_in(user, type = user.class.name)
     session[:user_id] = user.id
-    session[:user_type] = type
+    session[:user_type] = type.camelize
   end
 
   def current_user
