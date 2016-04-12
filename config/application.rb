52e648598fd9d6b0e3c4d57672cba542a626bed2
Daniel Latypov <dlatypov@lily>
2016-04-12 01:18:03 -0500

    WIP: added stuff to app.rb and got it to work?

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rack-cas'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,13 @@ module AgencyProjectCollectionSystem
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #todo: dunno what this is fully
+    config.assets.enabled = true
+    config.assets.version = '1.0'
+    #other project says it filters passwords from logs
+    config.filter_parameters += [:password]
+    require 'rack-cas/session_store/active_record'
+    config.rack_cas.session_store = RackCAS::ActiveRecordStore
   end
 end
