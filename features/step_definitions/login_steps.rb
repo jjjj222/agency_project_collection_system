a2f4bf6d935a8196c7d9981084507c57c4c82f64
jjjj222 <jjjj222.tso@gmail.com>
2016-04-19 02:42:05 -0500

    Support "Login as an Agency"

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -11,3 +11,23 @@ Given /the Google user exist/ do |user_table|
 
   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(auth_hash)
 end
+
+Given /^I am logged in as (?:a|an) (.+)$/i do |user_type|
+  if user_type.downcase == "agency"
+    info_hash = Hash.new
+    info_hash['name'] = 'Test User'
+    info_hash['email'] = 'test@gmail.com'
+
+    auth_hash = Hash.new
+    auth_hash['info'] = info_hash
+    auth_hash['provider'] = 'google_oauth2'
+    auth_hash['uid'] = '11111'
+
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(auth_hash)
+
+    visit root_path
+    click_link "Login"
+    click_link "Sign in with Google"
+  end
+end
+
