77332233bbd4a5e0fb46df16813eef703323b576
Daniel Latypov <dlatypov@lily>
2016-04-19 21:43:32 -0500

    ensure_logged_in -> ensure_cas_logged_in
    * changed it so that testing stage doesn't have to fake cas info as well
      for logging in
    
    * todo: need to handle corner cases where agency logged in but it
      needs a tamu user

--- app/controllers/cas_authenticated_controller.rb
+++ app/controllers/cas_authenticated_controller.rb
@@ -1,11 +1,12 @@
 class CasAuthenticatedController < ApplicationController
   include SessionsHelper
 
-  def ensure_logged_in
+  def ensure_cas_logged_in
     # Unfortunately, we can't access the parameters in this call even after it
     # returns from authentication, so the logic to use the info to log in the
     # user is done in SessionsHelper#logged_in?
-    if not cas_logged_in?
+    if not logged_in?
+      #TODO: handle when logged in as agency
       render status: 401, text: "Redirecting to SSO..."
     end
   end
--- spec/controllers/cas_authenticated_controller_spec.rb
+++ spec/controllers/cas_authenticated_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe CasAuthenticatedController, type: :controller do
 
-  describe "ensure_logged_in" do
+  describe "ensure_cas_logged_in" do
   end
 
 end
