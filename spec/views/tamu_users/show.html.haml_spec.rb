eec018beaf73e3a94aaeabfcf3353118bad5b1a9
Daniel Latypov <dlatypov@lily>
2016-04-25 13:10:07 -0500

    use project presenter, show agency in users projs

--- spec/views/tamu_users/show.html.haml_spec.rb
+++ spec/views/tamu_users/show.html.haml_spec.rb
@@ -15,14 +15,16 @@ describe 'tamu_users/show.html.haml' do
   end
   
   it 'displays a users projects correctly' do
+    agency = FactoryGirl.create(:agency, :default, :approved)
     assign(:tamu_user, FactoryGirl.build(:tamu_user, :default, :id=>1,
-        :projects => [FactoryGirl.build(:project, :default, :id=>1)]
+        :projects => [FactoryGirl.build(:project, :default, id: 1, agency: agency)]
         ))
 
     render
 
     expect(rendered).to include('Test Project')
     expect(rendered).to include('This is the test project description')
-    expect(rendered).to include('open')
+    expect(rendered).to match(/open/i)
+    expect(rendered).to include('Test Agency')
   end
 end
