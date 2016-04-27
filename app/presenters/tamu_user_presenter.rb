80d459e7a3e00f2524f607d32da4aa622ead7a55
Daniel Latypov <dlatypov@lily>
2016-04-27 12:59:57 -0500

    add table of users working on proj with name/role
    * also prettified the role to show up as faculty

--- app/presenters/tamu_user_presenter.rb
+++ app/presenters/tamu_user_presenter.rb
@@ -4,6 +4,17 @@ class TamuUserPresenter < PresenterBase
   delegate :admin?, :id, to: :tamu_user
   present_with :capitalize, :role
 
+  def role
+    case tamu_user.role
+    when /unapproved_professor/i
+      "Faculty (Unapproved)"
+    when /professor/i
+      "Faculty"
+    else
+      "Student"
+    end
+  end
+
   def profile
     link_to tamu_user.name, tamu_user_path(tamu_user)
   end
