75fe33a2ce4e48cd8b3358372a65fc134ce8ab2c
jjjj222 <jjjj222.tso@gmail.com>
2016-05-02 16:13:03 -0500

    Add image to agency profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429201121) do
+ActiveRecord::Schema.define(version: 20160502210123) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160429201121) do
     t.boolean  "approved",     default: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "image_url"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -53,4 +54,5 @@ ActiveRecord::Schema.define(version: 20160429201121) do
     t.boolean  "master_admin", default: false
     t.boolean  "blocked",      default: false
   end
+
 end
